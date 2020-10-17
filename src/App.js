8b2963cd47e1ebdc91d9c49536c60210802d13d4
Chandrasekhar Samanta <56759909+chandrasekhar2039@users.noreply.github.com>
2020-10-17 07:44:06 +0530

    login portal completed. work on dashboard is left

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import Home from "./components/home"
+import Home from "./components/home";
+import Dashboard from "./components/dashboard"
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Swal from 'sweetalert2';
 
 function App(){
-  return <div><Home /></div>
+  return <div>
+  <Router>
+  <Route path="/" exact>
+  <Home />
+  </Route>
+  <Route path="/dashboard" exact component = {Dashboard} />
+  </Router>
+  </div>
 }
 
 export default App;
--- src/components/input.js
+++ src/components/input.js
@@ -1,7 +1,12 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useHistory,Link } from "react-router-dom";
+import Swal from 'sweetalert2';
 
 function Input(){
+  //creating history constant
+  let history = useHistory();
+  //creating log state
   const [log,setlog]=React.useState(true);
   // state for inpute username
   const [user,setuser]=React.useState("");
@@ -20,11 +25,32 @@ function submit(event){
 
   if(log){
     let get_data=JSON.parse(localStorage.getItem("users"));
+    if(get_data !==null){
     get_data.map(found=>{
       if(found.username===user&& found.password===password){
-        alert("found");
-      }else{console.log("not found");}
-    });
+        setuser("");
+        setpassword("");
+        history.push("/dashboard");
+      }else{
+        Swal.fire({
+        icon: 'error',
+        title: 'User not found',
+        text: 'Check your email or phone number and password.',
+        footer: '<a href>Create a new Account.</a>' // log value need to change to perform new account trigger
+})
+setuser("");
+setpassword("");
+      }
+    })} else{
+      Swal.fire({
+      icon: 'error',
+      title: 'User not found',
+      text: 'Check your details and try again.',
+      footer: "<a href='../'>Create a new Account.</a>" // log value need to change to perform new account trigger
+})
+setuser("");
+setpassword("");
+    }
   } else{
     var data={
       username:user,
@@ -34,16 +60,21 @@ function submit(event){
     var user_data=JSON.parse(localStorage.getItem("users") || "[]");
     user_data.push(data);
     localStorage.setItem("users",JSON.stringify(user_data));
-    alert("submitted");
     setuser("");
     setpassword("");
+    Swal.fire({
+    icon: 'success',
+    title: 'congratulations',
+    text: 'You have successfully signed up',
+  });
+  history.push("/dashboard");
   }
 }
   return <div className="input">
 <form onSubmit={submit}>
 <input type="text" name="username" placeholder="Email adress or phone number" autoComplete="off" required autoFocus="1" onChange={call_user} value={user}/>
 <input type="password" name="password" placeholder="Password" autoComplete="off" required onChange={call_pass} value={password}/><br/>
-<button className="btn btn-primary btn-lg" type="submit"><strong>{log?"log in":"Create new Account"}</strong></button>
+<button className="btn btn-primary btn-lg butt" type="submit"><strong>{log?"log in":"Create new Account"}</strong></button>
 </form>
 { log && <>
 <div className="forgot_passsword"><a href="#" >Forgotten password?</a></div>
